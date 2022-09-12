d4d2adeb9d113fb47c7e7527ba2e472aec9be523
Mihaylova Marina <Infinity2703@yandex.ru>
2022-09-13 00:26:14 +0300

    Домашнее задание к 3 уроку

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+
+/*Console.Write("Введите число ");
+string number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3])
+  {
+    Console.WriteLine($"Число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Число: {number} - НЕ палиндром.");
+}
+ CheckingNumber(number); */
+
+
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+/* Console.Write("Введите координату точки А по оси x: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки А по оси y: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки А по оси z: ");
+int Az = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки B по оси x: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки B по оси y: ");
+int By = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату точки B по оси z: ");
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+double lenghtAB = Math.Sqrt(((Bx-Ax)*(Bx-Ax)+(By-Ay)*(By-Ay))+(Bz-Az)*(Bz-Az));
+
+Console.WriteLine("Длина отрезка АВ: " + lenghtAB); */
+
+
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+//кубов чисел от 1 до N.
+
+/*Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int cube = 1;
+while (cube <= number)
+{
+    int result = cube * cube * cube;
+    Console.Write(" " + result);
+    cube++;
+}
+*/
